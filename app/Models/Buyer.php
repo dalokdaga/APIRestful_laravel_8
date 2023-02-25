cfff24f0dfd7e35947109a0156dab288d4a29664
dalokdaga <dalokdaga@gmail.com>
2023-02-24 23:18:29 -0600

    implementacion de modelos video 39

--- app/Models/Buyer.php
+++ app/Models/Buyer.php
@@ -1,11 +1,13 @@
 <?php
 
 namespace App\Models;
-
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Transaction;
 use Illuminate\Database\Eloquent\Model;
 
 class Buyer extends Model
 {
-    use HasFactory;
+    public function transactions()
+    {
+        return $this->hasMany(Transaction::class);
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -12,6 +12,11 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
+    const USUARIO_VERIFICADO = '1';
+    const USUARIO_NO_VERIFICADO = '0';
+
+    const USUARIO_ADMINISTRADOR = 'true';
+    const USUARIO_REGULAR = 'false';
     /**
      * The attributes that are mass assignable.
      *
@@ -21,6 +26,9 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'verified',
+        'verification_token',
+        'admin',
     ];
 
     /**
@@ -31,6 +39,7 @@ class User extends Authenticatable
     protected $hidden = [
         'password',
         'remember_token',
+        'verification_token'
     ];
 
     /**
@@ -41,4 +50,16 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function esVerificado(){
+        return $this->verified == User::USUARIO_VERIFICADO;
+    }
+
+    public function esAdminstrador(){
+        return $this->admin == User::USUARIO_ADMINISTRADOR;
+    }
+    
+    public function generarVerificationToken(){
+        return str_random(40);
+    }
 }
